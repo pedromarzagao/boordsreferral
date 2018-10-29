336a4deea0df0ea86d1d21c2f6f380973d854589
Pedro Marzagao <pedromarzagao@gmail.com>
2018-10-29 00:21:18 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get '/auth/:user_id/:token', to: 'session#auth'
+  get '/join', to:'users#join'
+  get '/register', to: 'users#register'
+  get 'static/home'
+  get 'pages/home'
+  get 'pages/help'
+  root 'static#home'
+  get '/resend_login', to:'users#resend_login'
+  post '/resend_login', to:'users#resend_login_link'
+
+  resources :session, only: [:new, :create]
+  resources :users, only: [:create, :show, :resend_login_link]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
